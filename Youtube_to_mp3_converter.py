71792c199b707b1be0de5a83f84570f8a0acf744
Rxmi-sk <Rslimk3@gmail.com>
2022-12-24 11:55:47 -0500

    program complete

--- Youtube_to_mp3_converter.py
+++ Youtube_to_mp3_converter.py
@@ -2,3 +2,17 @@ import youtube_dl
 
 def youtube_to_mp3():
     vid_link=input("Please enter the Youtube video's link:")
+    vid_information=youtube_dl.YoutubeDL().extract_info(url = vid_link,download=False)
+    filename=f"{vid_information['title']}.mp3"
+
+    options={
+        'format':'bestaudio/best',
+        'keepvideo':False,
+        'outtmpl':filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([vid_information['webpage_url']])
+
+    print("Download complete... {}".format(filename))
+    
